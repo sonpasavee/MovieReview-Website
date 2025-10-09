5947f189225955a3d7bb14c04d3e5ee72f146845
sonpasavee975 <sunpasavee@gmail.com>
2025-10-10 00:44:21 +0700

    Updated 👍 : Admin

--- controllers/adminDashboardController.js
+++ controllers/adminDashboardController.js
@@ -1,3 +1,11 @@
-module.exports = (req , res) => {
-    res.render('adminDashboard')
+const Review = require('../models/Review')
+
+module.exports = async (req , res) => {
+    try {
+        const allReview = await Review.find()
+        
+    }catch(err) {
+
+    }
+    res.render('adminDashboard' , { allReview })
 }
