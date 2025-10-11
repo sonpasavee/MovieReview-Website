849ea34d01c5a8558b2fb8cf1f0ef2ac7e4afc99
sonpasavee975 <sunpasavee@gmail.com>
2025-10-11 21:48:33 +0700

    Updated 👍 : Collection.ejs collectionController

--- controllers/controllerCollection.js
+++ controllers/controllerCollection.js
@@ -1,5 +1,8 @@
 const Collection = require('../models/Collection')
-const Movie = require('../models/Movie')
+const axios = require('axios')
+const dotenv = require('dotenv')
+const TMDB_BASE_URL = "https://api.themoviedb.org/3/movie"
+dotenv.config()
 
 module.exports = async (req, res) => {
   try {
@@ -11,9 +14,22 @@ module.exports = async (req, res) => {
 
     const collections = await Collection.find({ userId: user._id })
 
-    const collectionsWithMovies = await Promise.all(collections.map(async (c) => {
-      const movies = await Movie.find({ movieId: { $in: c.movies } })
-      return { ...c.toObject(), movies }
+     const collectionsWithMovies = await Promise.all(collections.map(async (c) => {
+      const movies = await Promise.all(c.movies.map(async (tmdbId) => {
+        try {
+          const response = await axios.get(`${TMDB_BASE_URL}/${tmdbId}`, {
+            params: { api_key: process.env.TMDB_KEY, language: 'th-TH' } // เปลี่ยนภาษาได้
+          })
+          return response.data
+        } catch (err) {
+          console.error(`Error fetching movie ${tmdbId}`, err.message)
+          return null
+        }
+      }))
+
+      // กรอง movie ที่ไม่สำเร็จ
+      const validMovies = movies.filter(m => m !== null)
+      return { ...c.toObject(), movies: validMovies }
     }))
 
     res.render('collection', { collections: collectionsWithMovies, loggedIn: req.session.userId })
@@ -58,12 +74,12 @@ module.exports.renameCollection = async (req, res) => {
   const user = res.locals.UserData
   if (!user) return res.status(401).send('Unauthorized')
 
-  const { id } = req.params
+  const { collectionId } = req.params
   const { name } = req.body
   if (!name || name.trim() === '') return res.status(400).send('Name is required')
 
   try {
-    await Collection.updateOne({ _id: id, userId: user._id }, { name: name.trim() })
+    await Collection.updateOne({ _id: collectionId , userId: user._id }, { name: name.trim() })
     res.sendStatus(200)
   } catch (err) {
     console.error(err)
